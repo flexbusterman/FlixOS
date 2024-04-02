63572c21eb0f6a2f04f9834c3b58d4f6a6084cfd
Christian Augustin <johnchristianaugustin@gmail.com>
2024-04-02 02:09:37 +0200

    NetworkManager

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -146,15 +146,14 @@ services.mullvad-vpn.enable = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Enable networking
   networking.hostName = "Legion5"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Stockholm";
@@ -316,6 +315,7 @@ users.defaultUserShell = pkgs.zsh;
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+		maim
 		# for awesomewm
     luajitPackages.luarocks
 		luajitPackages.luadbi
